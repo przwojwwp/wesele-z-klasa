7f39fa81db46b670a4cb8c74c0786f8509c54dd5
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-10-25 14:07:02 +0200

    feat: add header component with basic structure and styles

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,15 @@
+// src/app/shared/components/header/header.ts
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss',
+  styleUrls: ['./header.scss'],
 })
-export class Header {
-
-}
+export class HeaderComponent {}
