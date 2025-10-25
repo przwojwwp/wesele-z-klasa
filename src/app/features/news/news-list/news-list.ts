d1053a87a753235297f5d0c853d22b1c87ab85a9
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-10-25 13:15:35 +0200

    chore: initial setup for news app

---
+++ src/app/features/news/news-list/news-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-list',
+  imports: [],
+  templateUrl: './news-list.html',
+  styleUrl: './news-list.scss',
+})
+export class NewsList {
+
+}
