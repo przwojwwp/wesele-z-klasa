28a01b1d37ecf4f9e7c8c2ae218c8fdc1be4641d
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-10-26 21:00:47 +0100

    feat: header conditional rendering

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal, inject } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header';
-import { LoginComponent } from "./features/login/login";
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('news-app');
+  protected readonly loggedIn = signal(false);
+  private auth = inject(AuthService);
+
+  ngOnInit() {
+    this.auth.getUser().subscribe((user) => {
+      this.loggedIn.set(!!user);
+    });
+  }
 }
